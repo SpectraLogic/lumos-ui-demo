8147270764a7163afada116455e18630b9a3e59d
parallels <russell.howell.94@gmail.com>
2022-03-04 20:27:56 -0500

    Updated Inner List Button

--- src/components/Nav/NavInner/ListButton.tsx
+++ src/components/Nav/NavInner/ListButton.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, ButtonProps, Typography } from '@mui/material';
+import { Button, ButtonProps, selectClasses, SvgIconProps, Typography } from '@mui/material';
 import styled from 'styled-components';
 import { InnerSelection } from '../Navigator';
 import { PartitionIcon, LifecycleIcon, NetworkIcon } from '../../Icons';
@@ -20,22 +20,35 @@ const StyledButton = styled(Button)<{selected: boolean}>`
     color: ${ ({selected}) => selected ? '#fff' : '#000' };
     &:hover{
         box-shadow: none;
+        background-color: ${ ({selected}) => selected ? '#000' : '#fff' };
     }
 `;
 
-//switch statement here?
+const ButtonIcon: React.FunctionComponent<{ selected: boolean, which: InnerSelection}> = (props) =>{
+    const iconProps: SvgIconProps =  { htmlColor: props.selected ? '#fff' : '#000' } ;
+    switch( props.which ){
+        case InnerSelection.Partitions: 
+            return ( <PartitionIcon { ...iconProps } />  );
+        case InnerSelection.MediaLifecycle:
+            return ( <LifecycleIcon { ...iconProps } />)
+        default: 
+            return ( <> </> )
+    }
+}
+    
 
 
 const ListButton: React.FunctionComponent<IListButtonProps> = (props) => {
     const selected = props.selection === props.which;
+    
   return (
     <StyledButton 
     { ...props }
     selected={ selected }
-    startIcon={ <PartitionIcon htmlColor='#fff' /> }
+    startIcon={ <ButtonIcon which={ props.which } selected={ selected } /> }
     variant='contained'> 
         <Typography variant='body1'>
-            Partitions
+            { props.which }
         </Typography>
     </StyledButton>
   ) ;
