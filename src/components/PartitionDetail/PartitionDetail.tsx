cf5288e3b4c698dad001fe7eb3a8a73589c97415
parallels <russell.howell.94@gmail.com>
2022-03-17 17:47:44 -0600

    Ability to discard changes to a Partition

--- src/components/PartitionDetail/PartitionDetail.tsx
+++ src/components/PartitionDetail/PartitionDetail.tsx
@@ -1,4 +1,5 @@
-import { Divider } from '@mui/material';
+import { CheckCircle, Clear } from '@mui/icons-material';
+import { Button, ButtonGroup, Divider, Typography } from '@mui/material';
 import _ from 'lodash';
 import * as React from 'react';
 import styled from 'styled-components';
@@ -17,6 +18,7 @@ interface IPartitionDetailProps {
     availablePartitions: Array<IPartition>
     partition: IPartition
     partitionId: string
+    onChange?: ( partition: IPartition ) => void
 }
 
 const Root = styled.div`
@@ -39,6 +41,11 @@ const Body = styled.div`
     overflow-y: scroll;
 `;
 
+const ForceExtraScroll = styled.div`
+    width: 100%;
+    height: 56px;
+`;
+
 const HeaderLeft = styled.div`
     align-self: flex-start;
 `;
@@ -47,14 +54,31 @@ const HeaderRight = styled.div`
     margin-left: auto;
 `;
 
-const stagedEditsReducer = ( state: Partial<IPartition>, newState: Partial<IPartition> ) => ({
-    ...state,
-    ...newState
-})
+enum StagedEditsActions {
+    UPDATE,
+    CLEAR
+}
+
+const stagedEditsReducer = ( state: Partial<IPartition>, action: { type: StagedEditsActions, payload?: Partial<IPartition> } ) => {
+    switch( action.type ){
+        case StagedEditsActions.CLEAR:
+            return {}; 
+            break;
+        case StagedEditsActions.UPDATE:
+            return {
+                ...state,
+                ...action.payload
+            }
+            break;
+        default:
+            return state;
+    }
+}
+    
 
 
 const PartitionDetail: React.FunctionComponent<IPartitionDetailProps> = (props) => {
-    const [stagedEditState, setStagedEditState] = React.useReducer( stagedEditsReducer as React.Reducer<Partial<IPartition>, Partial<IPartition>>, {} as Partial<IPartition> );
+    const [stagedEditState, setStagedEditState] = React.useReducer( stagedEditsReducer as React.Reducer<Partial<IPartition>, { type: StagedEditsActions, payload: Partial<IPartition> }>, {} as Partial<IPartition> );
     const { 
         name, mediaType, 
         SlotIQ: slotIq, 
@@ -68,7 +92,7 @@ const PartitionDetail: React.FunctionComponent<IPartitionDetailProps> = (props) 
       <Root>
         <Header>
             <HeaderLeft>
-                <Name value={ _.get( stagedEditState, ["name"], name ) } onValueChange={ ( value ) => setStagedEditState({ name: value }) } />
+                <Name value={ _.get( stagedEditState, ["name"], name ) } onValueChange={ ( value ) => setStagedEditState({ type: StagedEditsActions.UPDATE, payload: { name: value } }) } />
             </HeaderLeft>
             <HeaderRight>
                 right
@@ -77,38 +101,61 @@ const PartitionDetail: React.FunctionComponent<IPartitionDetailProps> = (props) 
         <Body>
             <Divider /> 
             <MediaType 
-                value={ _.get( stagedEditState, ["mediaType"], mediaType ) } onValueChange={ value => setStagedEditState({ mediaType: value }) }
+                value={ _.get( stagedEditState, ["mediaType"], mediaType ) } onValueChange={ value => setStagedEditState({type: StagedEditsActions.UPDATE, payload: { mediaType: value } }) }
             />
             <SlotIQ 
-                value={_.get( stagedEditState, [PartitionFields.SlotIQ], slotIq ) } onValueChange={ (value: boolean) => setStagedEditState({ SlotIQ: value }) } 
+                value={_.get( stagedEditState, [PartitionFields.SlotIQ], slotIq ) } onValueChange={ (value: boolean) => setStagedEditState({ type: StagedEditsActions.UPDATE, payload: { SlotIQ: value } }) } 
             /> 
             <BarcodeOptions 
                 value={ _.get( stagedEditState, [PartitionFields.BarcodeOptions], barcodeOptions ) }
-                onValueChange={ (value: IBarcodeOptions) => setStagedEditState({ "Barcode Options": value }) } 
+                onValueChange={ (value: IBarcodeOptions) => setStagedEditState({ type: StagedEditsActions.UPDATE, payload: { "Barcode Options": value } }) } 
             />
             <Chambers
                 mediaType={ mediaType }
                 value={ _.get( stagedEditState, [PartitionFields.Chambers], chambers ) }
-                onValueChange={ (value: IMediaChambers | ICleaningChambers ) => setStagedEditState({ Chambers: value }) } 
+                onValueChange={ (value: IMediaChambers | ICleaningChambers ) => setStagedEditState({ type: StagedEditsActions.UPDATE, payload: {Chambers: value} }) } 
             />
             <Drives 
                 value={ _.get( stagedEditState, [PartitionFields.Drives], drives ) }
-                onValueChange={ (value: Array<string>) => setStagedEditState({ Drives: value }) } 
+                onValueChange={ (value: Array<string>) => setStagedEditState({ type: StagedEditsActions.UPDATE, payload: {Drives: value} }) } 
             />
             { props.partition.mediaType !== MediaTypeEnum.LTOClean && (
                 <CleaningPartition 
                     availablePartitions={ props.availablePartitions }
                     value={ _.get( stagedEditState, [PartitionFields.CleaningPartition], cleaningPartition ) }
-                    onValueChange={ (value: string | false ) => setStagedEditState({  "Cleaning Partition": value }) } 
+                    onValueChange={ (value: string | false ) => setStagedEditState({ type: StagedEditsActions.UPDATE, payload: {"Cleaning Partition": value} }) } 
                 />
             ) }
             <MLMVerification 
                 value={ _.get( stagedEditState, [PartitionFields.MLMVerification], mlmVerification ) }
-                onValueChange={ (value: IMLMVerificationConfig ) => setStagedEditState({  [PartitionFields.MLMVerification]: value }) } 
+                onValueChange={ (value: IMLMVerificationConfig ) => setStagedEditState({  type: StagedEditsActions.UPDATE, payload: {[PartitionFields.MLMVerification]: value} }) } 
 
             />
-            
-        </Body>   
+            { !_.isEmpty( stagedEditState ) && ( <ForceExtraScroll /> ) } 
+        </Body>
+        { !_.isEmpty( stagedEditState ) && (
+            <ButtonGroup fullWidth sx={{ transform: "translate(0, -56px)" }}>
+                <Button 
+                sx={{ height: "56px", borderRadius: "0 0 0 16px" }} 
+                onClick={ () => setStagedEditState( { type: StagedEditsActions.CLEAR, payload: {} } ) }
+                color='error'
+                variant='contained'
+                startIcon={ <Clear /> }>
+                    <Typography variant='body1'>
+                        Discard Changes
+                    </Typography>
+                </Button>
+                <Button 
+                sx={{ height: "56px", borderRadius: "0 0 16px 0" }} 
+                color='success'
+                variant='contained'
+                endIcon={ <CheckCircle /> }>
+                    <Typography variant='body1'>
+                        Save Changes
+                    </Typography>
+                </Button>
+            </ButtonGroup>   
+        ) }
       </Root>
   ) ;
 };
